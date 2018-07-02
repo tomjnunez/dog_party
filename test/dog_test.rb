b7e4932463a48128a3496b6cfa69e61f36bb323e
Tom Nunez <tomjnunez@gmail.com>
2018-07-02 13:49:18 -0600

    Create test for attributes

--- test/dog_test.rb
+++ test/dog_test.rb
@@ -10,4 +10,32 @@ class DogTest < Minitest::Test
 
     assert_instance_of Dog, dog
   end
+
+
+  def test_it_has_a_name
+   dog = Dog.new("Fido", 2, "Dalmation")
+
+   expected = "Fido"
+   actual   = dog.name
+
+   assert_equal expected, actual
+  end
+
+  def test_it_has_an_age
+   dog = Dog.new("Fido", 2, "Dalmation")
+
+   expected = 2
+   actual   = dog.age
+
+   assert_equal expected, actual
+  end
+
+  def test_it_has_a_breed
+   dog = Dog.new("Fido", 2, "Dalmation")
+
+   expected = "Dalmation"
+   actual   = dog.breed
+
+   assert_equal expected, actual
+  end
 end
